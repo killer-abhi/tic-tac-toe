75f3b7c5e15d78cb3b111e8fa568e4504e95691e
Sunder Kumar <sunderkumar2506@gmail.com>
2022-11-13 21:18:58 +0530

    first commit

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
-let Player1Mark="O";
+let Player1Mark = "O";
 let CurrentMark;
+const childCell = document.getElementsByClassName("cell-child");
 
 // let Filledcells = [-1, -1, -1, -1, -1, -1, -1, -1, -1];
 let Filledcells = new Array(9);
@@ -17,83 +18,75 @@ hoverO.src = "./assets/images/icon-o-outline.svg";
 hoverX.src = "./assets/images/icon-x-outline.svg";
 
 function handlePlayerMark(selected) {
-    if (selected === 0) {
-        Player1Mark = "O";
-    }
-    else {
-        Player1Mark = "X";
-    }
-    let previousSelected;
-    if (selected === 0) {
-        previousSelected = document.getElementsByClassName("mark")[1];
-    }
-    else {
-        previousSelected = document.getElementsByClassName("mark")[0];
-    }
-    previousSelected.classList.remove("mark-active");
-    let selectedMark = document.getElementsByClassName("mark")[selected];
-    selectedMark.classList.add("mark-active");
+  if (selected === 0) {
+    Player1Mark = "O";
+  } else {
+    Player1Mark = "X";
+  }
+  let previousSelected;
+  if (selected === 0) {
+    previousSelected = document.getElementsByClassName("mark")[1];
+  } else {
+    previousSelected = document.getElementsByClassName("mark")[0];
+  }
+  previousSelected.classList.remove("mark-active");
+  let selectedMark = document.getElementsByClassName("mark")[selected];
+  selectedMark.classList.add("mark-active");
 }
 function handleGame(type) {
-    CurrentMark=Player1Mark;
-    document.getElementById("img-Mark").innerHTML=imgO;
-    document.getElementsByClassName("start-game")[0].style.display = "none";
-    document.getElementsByClassName("game")[0].style.display="flex";
-
+  CurrentMark = Player1Mark;
+  document.getElementById("img-Mark").innerHTML = imgO;
+  document.getElementsByClassName("start-game")[0].style.display = "none";
+  document.getElementsByClassName("game")[0].style.display = "flex";
 }
 
 function changePlayer() {
-    if (CurrentMark == "O") {
-        document.getElementById("img-Mark").classList.remove("turn-O");
-        CurrentMark = "X";
-        document.getElementById("img-Mark").classList.add("turn-X");
-    }
-    else {
-        document.getElementById("img-Mark").classList.remove("turn-X");
-        CurrentMark = "O";
-        document.getElementById("img-Mark").classList.add("turn-O");
-    }
+  //   console.log(childCell);
+  console.log(CurrentMark);
+  if (CurrentMark == "O") {
+    // document.getElementById("img-Mark").classList.remove("turn-O");
+    childCell[0].classList.toggle("y");
+    CurrentMark = "X";
+    // document.getElementById("img-Mark").classList.add("turn-X");
+  } else {
+    // document.getElementById("img-Mark").classList.remove("turn-X");
+    CurrentMark = "O";
+    // document.getElementById("img-Mark").classList.add("turn-O");
+    childCell[0].classList.toggle("y");
+  }
 }
 function handleClick(index) {
-
-    if (CurrentMark === "O") {
-        Filledcells[index] = 0;
-        document.getElementsByClassName("cell")[index].appendChild(imgO);
-    }
-    else {
-        Filledcells[index] = 1;
-        document.getElementsByClassName("cell")[index].appendChild(imgX);
-    }
-    changePlayer();
+  if (CurrentMark === "O") {
+    Filledcells[index] = 0;
+    document.getElementsByClassName("cell")[index].appendChild(imgO);
+  } else {
+    Filledcells[index] = 1;
+    document.getElementsByClassName("cell")[index].appendChild(imgX);
+  }
+  changePlayer();
 }
 function checkHover(index) {
-    for (var i = 0; i < Filledcells.length; i++) {
-        if (Filledcells[index] === -1) {
-            return (1);
-        }
-        else {
-            return (0);
-        }
+  for (var i = 0; i < Filledcells.length; i++) {
+    if (Filledcells[index] === -1) {
+      return 1;
+    } else {
+      return 0;
     }
+  }
 }
 
 function handleHover(index) {
-
-    if (checkHover(index)) {
-        let hoveredCell = document.getElementsByClassName("cell")[index];
-        hoveredCell.style.cursor = "pointer";
-        if (CurrentMark === "O") {
-            let child=document.getElementsByClassName("cell-child")[0];
-            child.classList.add("o");
-            
-        } else {
-            hoveredCell.appendChild(hoverX);
-        }
-        hoveredCell.addEventListener("click", function () {
-            handleClick(index);
-            console.log("clicked")
-        })
+  if (checkHover(index)) {
+    let hoveredCell = document.getElementsByClassName("cell")[index];
+    hoveredCell.style.cursor = "pointer";
+    if (CurrentMark === "O") {
+      console.log(document.getElementsByClassName("x")[0]);
+    } else {
+      hoveredCell.appendChild(hoverX);
     }
+    hoveredCell.addEventListener("click", function () {
+      handleClick(index);
+      console.log("clicked");
+    });
+  }
 }
-
-
